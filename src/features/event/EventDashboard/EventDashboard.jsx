07cc78459c586fa9ac82677f21916272891bb60b
Emiliano Mastragostino <emiliano.mastra@gmail.com>
2020-03-24 10:44:52 +0100

    Added controlled form

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Grid, Button } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
+import cuid from 'cuid';
 
 const eventsFromDashboard = [
   {
@@ -61,21 +62,40 @@ class EventDashboard extends Component {
   };
 
   handleIsOpenToggle = () => {
-      this.setState(({isOpen}) => ({
-          isOpen: !isOpen
-      }))
-  }
+    this.setState(({ isOpen }) => ({
+      isOpen: !isOpen
+    }));
+  };
+
+  handleCreateEvent = (newEvent) => {
+    newEvent.id = cuid();
+    newEvent.hostPhotoURL = '/assets/user.png';
+    this.setState(({ events }) => ({
+      events: [...events, newEvent],
+      isOpen: false
+    }));
+  };
 
   render() {
     const { events, isOpen } = this.state;
+    
     return (
       <Grid>
         <Grid.Column width={10}>
           <EventList events={events} />
         </Grid.Column>
         <Grid.Column width={6}>
-          <Button positive content='Create Event' onClick={this.handleIsOpenToggle} />
-          {isOpen && <EventForm cancelFormOpen={this.handleIsOpenToggle} />}
+          <Button
+            positive
+            content='Create Event'
+            onClick={this.handleIsOpenToggle}
+          />
+          {isOpen && (
+            <EventForm
+              cancelFormOpen={this.handleIsOpenToggle}
+              createEvent={this.handleCreateEvent}
+            />
+          )}
         </Grid.Column>
       </Grid>
     );
